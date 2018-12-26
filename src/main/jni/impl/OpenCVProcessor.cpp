91f98df192877a52598706f72a48e25a3efd96af
ouyangshaocong <ouyangshaocong@yy.com>
2018-12-26 10:38:39 +0800

    replage package name

--- src/main/jni/impl/OpenCVProcessor.cpp
+++ src/main/jni/impl/OpenCVProcessor.cpp
@@ -2,7 +2,7 @@
 // Created by OuyangShaocong on 2018/12/13.
 //
 
-#include "com_yy_opencv_OpenCVProcessor.h"
+#include "OpenCVProcessor.h"
 #include <opencv2/opencv.hpp>
 #include <android/bitmap.h>
 #include <iostream>
@@ -11,7 +11,7 @@
 using namespace cv;
 using namespace std;
 
-JNIEXPORT jobject JNICALL Java_com_yy_opencv_OpenCVProcessor_detectHoughCircles
+JNIEXPORT jobject JNICALL Java_com_lulu_lulubox_opencv_OpenCVProcessor_detectHoughCircles
         (JNIEnv *env, jobject, jobject bitmap,
         jfloat factor, jint x, jint y, jint w, jint h) {
     //获得ArrayList类引用
@@ -56,9 +56,9 @@ JNIEXPORT jobject JNICALL Java_com_yy_opencv_OpenCVProcessor_detectHoughCircles
         fprintf(stderr, "hough circles not found.");
         return list_obj;
     }
-    jclass hc_cls = env->FindClass("com/yy/opencv/HoughCircles");
+    jclass hc_cls = env->FindClass("com/lulu/lulubox/opencv/HoughCircles");
     if (hc_cls == NULL) {
-        fprintf(stderr, "class com/yy/opencv/HoughCircles not found.");
+        fprintf(stderr, "class com/lulu/lulubox/opencv/HoughCircles not found.");
         return list_obj;
     }
     jmethodID hc_init = env->GetMethodID(hc_cls, "<init>", "(FFF)V");
@@ -73,7 +73,7 @@ JNIEXPORT jobject JNICALL Java_com_yy_opencv_OpenCVProcessor_detectHoughCircles
     return list_obj;
 }
 
-JNIEXPORT jobject JNICALL Java_com_yy_opencv_OpenCVProcessor_matchTemplate
+JNIEXPORT jobject JNICALL Java_com_lulu_lulubox_opencv_OpenCVProcessor_matchTemplate
         (JNIEnv *env, jobject, jobject bitmap, jobject temp,
         jfloat factor, jint x, jint y, jint w, jint h, jint scale) {
     //获取image
@@ -136,9 +136,9 @@ JNIEXPORT jobject JNICALL Java_com_yy_opencv_OpenCVProcessor_matchTemplate
     int bottom = top + _temp.rows;
     jobject r_obj = env->NewObject(r_cls, r_init, left, top, right, bottom);
     //获得RectWithConfidence类引用
-    jclass rc_cls = env->FindClass("com/yy/opencv/RectWithConfidence");
+    jclass rc_cls = env->FindClass("com/lulu/lulubox/opencv/RectWithConfidence");
     if (rc_cls == NULL) {
-        fprintf(stderr, "class com/yy/opencv/RectWithConfidence not found.");
+        fprintf(stderr, "class com/lulu/lulubox/opencv/RectWithConfidence not found.");
         return NULL;
     }
     jmethodID rc_init = env->GetMethodID(rc_cls, "<init>", "(Landroid/graphics/Rect;F)V");
